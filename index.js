a4af296c19b98e67d9d08641c424b48f6f75d3cb
AcheronX. <richardhuynh53@gmail.com>
2025-11-07 13:32:36 -0800

    comment

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var app = express();
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+// http://expressjs.com/en/starter/static-files.html and thats all
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
